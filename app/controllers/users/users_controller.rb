a115d802e72ec7d6684b02df5599eba7a71a73dc
Ryan Off <ryan@ryanoff.com>
2012-05-23 18:55:23 -0700

    nothing accomplished

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -7,6 +7,17 @@ class Users::UsersController < ApplicationController
     @dive_count = Dive.where(:user_id => current_user.id).count
     total_minutes = Dive.where(:user_id => current_user.id).sum(:bottomtime)
     @dive_time = Time.at(total_minutes*60).utc.strftime("%H:%M") #=> "01:00:00"
+    
+#@temp = Time.zone.now
+#raise @temp
+
+#@dives_this_year = Dive.where(:date >= ?, Time.now.year)
+#@dives_this_year = Dive.where(:date.to_date.year => Time.now.year)
+
+#where(:created_at => params[:date].to_date.beginning_of_month..params[:date].to_date.end_of_month).
+
+
+raise @dives_this_year.inspect 
 
   end
   
